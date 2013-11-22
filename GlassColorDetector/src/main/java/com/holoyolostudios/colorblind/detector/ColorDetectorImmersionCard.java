37eb9c29c01a26d6a2e5403e9bb1f1e22d073f16
Daniel Velazco <velazcod@gmail.com>
2013-11-21 16:12:55 -0800

    First commit. Boilerplate GDK app structure

---
+++ GlassColorDetector/src/main/java/com/holoyolostudios/colorblind/detector/ColorDetectorImmersionCard.java
@@ -0,0 +1,16 @@
+package com.holoyolostudios.colorblind.detector;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ColorDetectorImmersionCard extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
