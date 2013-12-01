5b59ecac4769c9593e9ea98795d5710e78dc60be
Daniel Velazco <velazcod@gmail.com>
2013-11-30 22:58:34 -0800

    Clear out a few color names for now

--- GlassColorDetector/src/main/java/com/holoyolostudios/colorblind/detector/colors/ColorNameCache.java
+++ GlassColorDetector/src/main/java/com/holoyolostudios/colorblind/detector/colors/ColorNameCache.java
@@ -147,7 +147,7 @@ public class ColorNameCache {
         mColorList.add(new ColorName("Dark Salmon", 0xE9, 0x96, 0x7A));
         mColorList.add(new ColorName("Dark Sea Green", 0x8F, 0xBC, 0x8F));
         mColorList.add(new ColorName("Dark Slate Blue", 0x48, 0x3D, 0x8B));
-        mColorList.add(new ColorName("Dark Slate Gray", 0x2F, 0x4F, 0x4F));
+//        mColorList.add(new ColorName("Dark Slate Gray", 0x2F, 0x4F, 0x4F));
         mColorList.add(new ColorName("Dark Turquoise", 0x00, 0xCE, 0xD1));
         mColorList.add(new ColorName("Dark Violet", 0x94, 0x00, 0xD3));
         mColorList.add(new ColorName("Deep Pink", 0xFF, 0x14, 0x93));
@@ -192,7 +192,7 @@ public class ColorNameCache {
         mColorList.add(new ColorName("Lime Green", 0x32, 0xCD, 0x32));
         mColorList.add(new ColorName("Linen", 0xFA, 0xF0, 0xE6));
         mColorList.add(new ColorName("Magenta", 0xFF, 0x00, 0xFF));
-        mColorList.add(new ColorName("Maroon", 0x80, 0x00, 0x00));
+//        mColorList.add(new ColorName("Maroon", 0x80, 0x00, 0x00));
         mColorList.add(new ColorName("Medium Aqua Marine", 0x66, 0xCD, 0xAA));
         mColorList.add(new ColorName("Medium Blue", 0x00, 0x00, 0xCD));
         mColorList.add(new ColorName("Medium Orchid", 0xBA, 0x55, 0xD3));
@@ -228,13 +228,13 @@ public class ColorNameCache {
         mColorList.add(new ColorName("Red", 0xFF, 0x00, 0x00));
         mColorList.add(new ColorName("Rosy Brown", 0xBC, 0x8F, 0x8F));
         mColorList.add(new ColorName("Royal Blue", 0x41, 0x69, 0xE1));
-        mColorList.add(new ColorName("Saddle Brown", 0x8B, 0x45, 0x13));
+//        mColorList.add(new ColorName("Saddle Brown", 0x8B, 0x45, 0x13));
         mColorList.add(new ColorName("Salmon", 0xFA, 0x80, 0x72));
         mColorList.add(new ColorName("Sandy Brown", 0xF4, 0xA4, 0x60));
         mColorList.add(new ColorName("Sea Green", 0x2E, 0x8B, 0x57));
         mColorList.add(new ColorName("Sea Shell", 0xFF, 0xF5, 0xEE));
-        mColorList.add(new ColorName("Sienna", 0xA0, 0x52, 0x2D));
-        mColorList.add(new ColorName("Silver", 0xC0, 0xC0, 0xC0));
+//        mColorList.add(new ColorName("Sienna", 0xA0, 0x52, 0x2D));
+//        mColorList.add(new ColorName("Silver", 0xC0, 0xC0, 0xC0));
         mColorList.add(new ColorName("Sky Blue", 0x87, 0xCE, 0xEB));
         mColorList.add(new ColorName("Slate Blue", 0x6A, 0x5A, 0xCD));
         mColorList.add(new ColorName("Slate Gray", 0x70, 0x80, 0x90));
